a5091f8547ecf824eba46fdbf9a7ec95d12f5f92
Jackson <jackson@rakena.co.nz>
2022-06-14 20:38:09 +1200

    Update themeConstants.ts

--- themeConstants.ts
+++ themeConstants.ts
@@ -1,6 +1,6 @@
 const themeConstants = {
   darkModeContrastingColor: "rgb(38 38 38)",
-  darkModePageBackground: "rgb(26, 30, 30)",
+  darkModePageBackground: "rgb(26, 32, 44)",
 };
 
 export default themeConstants;
