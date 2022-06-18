712158d8e797630d6a0d3db08bfb0ed68c9b1b22
Jackson Rakena <jackson@rakena.co.nz>
2022-06-19 01:19:57 +1200

    Add copyright and credits

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,4 +1,5 @@
-import { Box, Code, Heading, Stack, Text } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Box, Code, Heading, Link, Stack, Text } from "@chakra-ui/react";
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
@@ -22,6 +23,17 @@ const Account: NextPage = () => {
             </Text>
           </Box>
         </Stack>
+        <Box mt={12}>
+          <Text>Gradekeeper is &copy; 2022 Animals With Cool Hats, Inc.</Text>
+          <Text>Gradekeeper is powered by open-source software.</Text>
+          <Text>
+            Gradekeeper is{" "}
+            <Link color="teal.500" href="https://github.com/jacksonrakena/gradekeeper" isExternal>
+              open-source <ExternalLinkIcon mx="2px" />
+            </Link>
+            .
+          </Text>
+        </Box>
       </Box>
     </>
   );
