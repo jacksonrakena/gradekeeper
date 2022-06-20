85c0cdc1793b4969bed249438efedd3fc62b673c
Jackson Rakena <jackson@rakena.co.nz>
2022-06-20 18:07:52 +1200

    Add copyright and privacy policy beta

--- components/app/main/CourseList.tsx
+++ components/app/main/CourseList.tsx
@@ -14,6 +14,7 @@ import {
   Button,
   Heading,
   IconButton,
+  Link,
   Modal,
   ModalBody,
   ModalContent,
@@ -45,161 +46,167 @@ const CourseList = () => {
   const [courseCreateBlockId, setCourseCreateBlockId] = useState("");
   const createBlockDisclosure = useDisclosure();
   return (
-    <div className="mb-12">
-      <TopBar />
-      <>
-        <Head>
-          <title>My courses</title>
-        </Head>
-        <AlertDialog
-          isOpen={deleteStudyBlock !== null}
-          leastDestructiveRef={cancelRef}
-          onClose={() => {
-            setDeleteStudyBlock(null);
-          }}
-        >
-          <AlertDialogOverlay>
-            <AlertDialogContent>
-              <AlertDialogHeader fontSize="lg" fontWeight="bold">
-                Delete block &lsquo;{deleteStudyBlock?.name}&rsquo;
-              </AlertDialogHeader>
-              <AlertDialogBody>
-                Are you <strong>sure</strong> you want to delete {deleteStudyBlock?.name}? <br />
-                This will delete <strong>{deleteStudyBlock?.subjects.length}</strong> courses.
-              </AlertDialogBody>
-              <AlertDialogFooter>
-                <Button
-                  ref={cancelRef}
-                  onClick={() => {
-                    setDeleteStudyBlock(null);
-                  }}
-                >
-                  Cancel
-                </Button>
-                <Button
-                  colorScheme="red"
-                  onClick={async () => {
-                    setIsDeletingSb(true);
-                    await fetch(`/api/block/${deleteStudyBlock?.id ?? ""}`, { method: "DELETE" });
-                    await context.redownload();
-                    toast({
-                      title: "Study block deleted.",
-                      description: deleteStudyBlock?.name + " deleted.",
-                      duration: 4000,
-                      isClosable: true,
-                      status: "success",
-                    });
-                    setIsDeletingSb(false);
-                    setDeleteStudyBlock(null);
-                  }}
-                  isLoading={isDeletingStudyBlock}
-                  ml={3}
-                >
-                  Delete
-                </Button>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialogOverlay>
-        </AlertDialog>
-        <CreateCourseModal blockId={courseCreateBlockId} isOpen={!!courseCreateBlockId} onClose={() => setCourseCreateBlockId("")} />
-        <CreateBlockModal isOpen={createBlockDisclosure.isOpen} onClose={createBlockDisclosure.onClose} />
-        <div>
-          <div className={"flex flex-col px-12"}>
-            <Skeleton isLoaded={(user && !isLoading) ?? false}>
-              {(!user?.studyBlocks || user?.studyBlocks?.length === 0) && (
-                <div>
+    <>
+      <div className="mb-12">
+        <TopBar />
+        <>
+          <Head>
+            <title>My courses</title>
+          </Head>
+          <AlertDialog
+            isOpen={deleteStudyBlock !== null}
+            leastDestructiveRef={cancelRef}
+            onClose={() => {
+              setDeleteStudyBlock(null);
+            }}
+          >
+            <AlertDialogOverlay>
+              <AlertDialogContent>
+                <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                  Delete block &lsquo;{deleteStudyBlock?.name}&rsquo;
+                </AlertDialogHeader>
+                <AlertDialogBody>
+                  Are you <strong>sure</strong> you want to delete {deleteStudyBlock?.name}? <br />
+                  This will delete <strong>{deleteStudyBlock?.subjects.length}</strong> courses.
+                </AlertDialogBody>
+                <AlertDialogFooter>
+                  <Button
+                    ref={cancelRef}
+                    onClick={() => {
+                      setDeleteStudyBlock(null);
+                    }}
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    colorScheme="red"
+                    onClick={async () => {
+                      setIsDeletingSb(true);
+                      await fetch(`/api/block/${deleteStudyBlock?.id ?? ""}`, { method: "DELETE" });
+                      await context.redownload();
+                      toast({
+                        title: "Study block deleted.",
+                        description: deleteStudyBlock?.name + " deleted.",
+                        duration: 4000,
+                        isClosable: true,
+                        status: "success",
+                      });
+                      setIsDeletingSb(false);
+                      setDeleteStudyBlock(null);
+                    }}
+                    isLoading={isDeletingStudyBlock}
+                    ml={3}
+                  >
+                    Delete
+                  </Button>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialogOverlay>
+          </AlertDialog>
+          <CreateCourseModal blockId={courseCreateBlockId} isOpen={!!courseCreateBlockId} onClose={() => setCourseCreateBlockId("")} />
+          <CreateBlockModal isOpen={createBlockDisclosure.isOpen} onClose={createBlockDisclosure.onClose} />
+          <div>
+            <div className={"flex flex-col px-12"}>
+              <Skeleton isLoaded={(user && !isLoading) ?? false}>
+                {(!user?.studyBlocks || user?.studyBlocks?.length === 0) && (
                   <div>
-                    <Box mb={6}>
-                      <Heading as="h2" size="md">
-                        Welcome to Gradekeeper.
-                      </Heading>
-                      <Heading size="sm">Let&apos;s get you set up.</Heading>
-                    </Box>
-                    <Box mb={6}>
-                      Gradekeeper is organised around a system of &lsquo;study blocks&rsquo;, which can be trimesters, semesters, or terms,
-                      depending on your university.
-                    </Box>
-                    <Box mb={6}>
-                      <Alert>
-                        <AlertIcon />
-                        <AlertDescription>Let&apos;s start by making a study block.</AlertDescription>
-                      </Alert>
-                    </Box>
-                    <Button colorScheme="orange" size="sm" onClick={createBlockDisclosure.onOpen}>
-                      + New study block
-                    </Button>
-                  </div>
-                </div>
-              )}
-              {user &&
-                user.studyBlocks &&
-                (user?.studyBlocks?.length ?? 0) > 0 &&
-                user?.studyBlocks?.map((studyBlock) => {
-                  const sbStart = new Date(studyBlock.startDate);
-                  const sbEnd = new Date(studyBlock.endDate);
-                  const dtf = new Intl.DateTimeFormat("en-US", {
-                    month: "long",
-                    day: "numeric",
-                    year: "numeric",
-                  });
-                  return (
-                    <div key={studyBlock.id} className="mb-12">
-                      <div className="flex">
-                        <Heading size="lg">
-                          {studyBlock.name}
-                          <IconButton
-                            onClick={() => {
-                              setDeleteStudyBlock(studyBlock);
-                            }}
-                            className="ml-4"
-                            icon={<DeleteIcon />}
-                            size="xs"
-                            aria-label={"Delete"}
-                            colorScheme="teal"
-                          />
+                    <div>
+                      <Box mb={6}>
+                        <Heading as="h2" size="md">
+                          Welcome to Gradekeeper.
                         </Heading>
-                      </div>
-                      <div className="text-md">
-                        {dtf.format(sbStart)} &#8212; {dtf.format(sbEnd)}
-                      </div>
-
-                      {user.studyBlocks.length === 1 && studyBlock.subjects.length === 0 && (
-                        <Box mt={4}>
-                          <Alert>
-                            <AlertIcon />
-                            <AlertTitle>Great job!</AlertTitle>
-                            <AlertDescription>Now, let&apos;s make a course in {studyBlock.name}.</AlertDescription>
-                          </Alert>
-                        </Box>
-                      )}
-
-                      {studyBlock.subjects.map((subject) => (
-                        <CoursePill
-                          key={subject.id}
-                          onClick={() => {
-                            router.push(`/blocks/${studyBlock.id}/courses/${subject.id}`);
-                          }}
-                          subject={subject}
-                          gradeMap={user.gradeMap}
-                        />
-                      ))}
-
-                      <Button onClick={() => setCourseCreateBlockId(studyBlock.id)} mt={4} colorScheme="teal" size="sm">
-                        + New course in {studyBlock.name}
+                        <Heading size="sm">Let&apos;s get you set up.</Heading>
+                      </Box>
+                      <Box mb={6}>
+                        Gradekeeper is organised around a system of &lsquo;study blocks&rsquo;, which can be trimesters, semesters, or
+                        terms, depending on your university.
+                      </Box>
+                      <Box mb={6}>
+                        <Alert>
+                          <AlertIcon />
+                          <AlertDescription>Let&apos;s start by making a study block.</AlertDescription>
+                        </Alert>
+                      </Box>
+                      <Button colorScheme="orange" size="sm" onClick={createBlockDisclosure.onOpen}>
+                        + New study block
                       </Button>
                     </div>
-                  );
-                })}
-              {user?.studyBlocks && user?.studyBlocks.length > 0 && (
-                <Button colorScheme="orange" size="sm" onClick={createBlockDisclosure.onOpen}>
-                  + New trimester/semester
-                </Button>
-              )}
-            </Skeleton>
+                  </div>
+                )}
+                {user &&
+                  user.studyBlocks &&
+                  (user?.studyBlocks?.length ?? 0) > 0 &&
+                  user?.studyBlocks?.map((studyBlock) => {
+                    const sbStart = new Date(studyBlock.startDate);
+                    const sbEnd = new Date(studyBlock.endDate);
+                    const dtf = new Intl.DateTimeFormat("en-US", {
+                      month: "long",
+                      day: "numeric",
+                      year: "numeric",
+                    });
+                    return (
+                      <div key={studyBlock.id} className="mb-12">
+                        <div className="flex">
+                          <Heading size="lg">
+                            {studyBlock.name}
+                            <IconButton
+                              onClick={() => {
+                                setDeleteStudyBlock(studyBlock);
+                              }}
+                              className="ml-4"
+                              icon={<DeleteIcon />}
+                              size="xs"
+                              aria-label={"Delete"}
+                              colorScheme="teal"
+                            />
+                          </Heading>
+                        </div>
+                        <div className="text-md">
+                          {dtf.format(sbStart)} &#8212; {dtf.format(sbEnd)}
+                        </div>
+
+                        {user.studyBlocks.length === 1 && studyBlock.subjects.length === 0 && (
+                          <Box mt={4}>
+                            <Alert>
+                              <AlertIcon />
+                              <AlertTitle>Great job!</AlertTitle>
+                              <AlertDescription>Now, let&apos;s make a course in {studyBlock.name}.</AlertDescription>
+                            </Alert>
+                          </Box>
+                        )}
+
+                        {studyBlock.subjects.map((subject) => (
+                          <CoursePill
+                            key={subject.id}
+                            onClick={() => {
+                              router.push(`/blocks/${studyBlock.id}/courses/${subject.id}`);
+                            }}
+                            subject={subject}
+                            gradeMap={user.gradeMap}
+                          />
+                        ))}
+
+                        <Button onClick={() => setCourseCreateBlockId(studyBlock.id)} mt={4} colorScheme="teal" size="sm">
+                          + New course in {studyBlock.name}
+                        </Button>
+                      </div>
+                    );
+                  })}
+                {user?.studyBlocks && user?.studyBlocks.length > 0 && (
+                  <Button colorScheme="orange" size="sm" onClick={createBlockDisclosure.onOpen}>
+                    + New trimester/semester
+                  </Button>
+                )}
+              </Skeleton>
+            </div>
           </div>
-        </div>
-      </>
-    </div>
+        </>
+      </div>
+      <Box mx={12} mb={4} color={"GrayText"} fontSize={"sm"}>
+        &copy; Animals With Cool Hats, Inc. <br />
+        <Link href="/legal/privacy">Privacy</Link> &bull; <Link href="/legal/feedback">Feedback</Link>
+      </Box>
+    </>
   );
 };
 
---
+++ pages/legal/privacy.tsx
@@ -0,0 +1,75 @@
+import { Box, Code, Heading, Link, Stack, Text } from "@chakra-ui/react";
+import { NextPage } from "next";
+
+const PrivacySubheading = ({ children }) => {
+  return <Heading size="md">{children}</Heading>;
+};
+
+const PrivacyPolicy: NextPage = () => {
+  return (
+    <Box m={12}>
+      <Stack spacing={8}>
+        <Stack spacing={2}>
+          <Heading>Privacy policy</Heading>
+          <Text size={"sm"}>Last updated: June 20th, 2022</Text>
+        </Stack>
+        <Box>
+          <Stack spacing={8}>
+            <Box>
+              <PrivacySubheading>Introduction &amp; definitions</PrivacySubheading>
+              Gradekeeper believes in the user's fundamental right to privacy.
+              <br />
+              <br />
+              For the purposes of this document, 'we' or 'us', refers to Animals With Cool Hats, Inc. (<Code>NZBN 9429048922678</Code>), a
+              registered business in New Zealand. <br />
+              'Gradekeeper' refers to Gradekeeper, an Internet service provided by us. <br />
+            </Box>
+            <Box>
+              <PrivacySubheading>Jurisdiction of operation and data</PrivacySubheading>
+              Gradekeeper is a product of Animals With Cool Hats, Inc. (<Code>NZBN 9429048922678</Code>), a registered sole trader business
+              in New Zealand.
+              <br />
+              <br />
+              User data can be processed in either Washington, D.C., United States, or Sydney, Australia by Gradekeeper's third-party
+              infrastructure provider, <Link href="https://vercel.com">Vercel</Link>.<br />
+              User data is persisted and stored in Sydney, Australia, by third-party data provider{" "}
+              <Link href="https://planetscale.com/">PlanetScale</Link>.
+            </Box>
+            <Box>
+              <PrivacySubheading>Usage of user data</PrivacySubheading>A user's login provider identifier (i.e. a Google email, GitHub
+              username, or Twitter name) is used to identify the user in Gradekeeper's systems, and persist data between sessions.
+              <br />
+              <br />
+              If a user has an email attached to their account login, Gradekeeper may send them Critical Service Notifications (CSNs) in
+              case of a serious concern or risk with their account or data. Gradekeeper will not use your email address for any other
+              purpose.
+            </Box>
+            <Box>
+              <PrivacySubheading>Collection of user data</PrivacySubheading>A user's Google identifier is collected and stored in
+              Gradekeeper's systems for the purpose of identifying the user and their data. The user's Google name and profile picture are
+              received from Google and presented to show the user their identity, but this data is not persisted in Gradekeeper's systems.
+            </Box>
+            <Box>
+              <PrivacySubheading>Advertising</PrivacySubheading>
+              In order to pay for infrastructure, marketing, and service costs, Gradekeeper may present advertisements through{" "}
+              <b>Google AdSense</b> on the Gradekeeper website. Gradekeeper does not intentionally share user information with Google
+              AdSense to target advertisements, and does not sell user information to advertising-related third parties.
+            </Box>
+            <Box>
+              <PrivacySubheading>Access to data by third-parties</PrivacySubheading>
+              As of June 20th, 2022, Gradekeeper does not share user data with third-parties.
+            </Box>
+            <Box>
+              <PrivacySubheading>The right to be forgotten</PrivacySubheading>
+              Gradekeeper does not currently have an automated service to delete user information. Users can delete all account data stored
+              by Gradekeeper by emailing <Code>support@animalswithcoolhats.com</Code> with <Code>DATA: Delete my Gradekeeper data</Code> in
+              the subject.
+            </Box>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default PrivacyPolicy;
