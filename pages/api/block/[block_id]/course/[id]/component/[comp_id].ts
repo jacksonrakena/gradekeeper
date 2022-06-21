2bc895b9be85eacd469b3e8aabc7420daade7859
Jackson Rakena <jackson@rakena.co.nz>
2022-06-21 23:19:32 +1200

    Fix an issue where a component could get into an invalid state

--- pages/api/block/[block_id]/course/[id]/component/[comp_id].ts
+++ pages/api/block/[block_id]/course/[id]/component/[comp_id].ts
@@ -16,54 +16,35 @@ export default gkRoute(async (req: NextApiRequest, res: NextApiResponse<object>)
       include: { subcomponents: true },
     });
     if (!targetComponent) return res.status(400);
-
-    const initupd = await primsa.subjectComponent.update({
-      where: {
-        id: comp_id.toString(),
-      },
-      data: {
-        ...req.body,
-        subcomponentsArray: undefined,
-        subcomponents: { deleteMany: [{ componentId: { not: "" } }] },
-      },
-    });
     if (req.body.subcomponents) {
-      const replacementSubcomponents = req.body.subcomponents.map((e: Partial<SubjectSubcomponent>) => {
-        return {
-          where: { id: e.id },
-          data: { ...e, componentId: undefined },
-        };
-      });
-      if (targetComponent.subcomponents.length === 0) {
-        const resultdata = await primsa.subjectComponent.update({
-          where: { id: targetComponent.id },
-          data: {
-            subcomponents: {
-              createMany: {
-                data: req.body.subcomponents,
-              },
-            },
-          },
-          include: { subcomponents: true },
-        });
-
-        if (resultdata) return res.status(200).json(resultdata);
-        return res.status(500);
-      }
-      const resultdata = await primsa.subjectComponent.update({
-        where: { id: comp_id.toString() },
+      const initupd = await primsa.subjectComponent.update({
+        where: {
+          id: comp_id.toString(),
+        },
         data: {
           ...req.body,
+          subcomponentsArray: undefined,
+          subcomponents: { deleteMany: [{ componentId: { not: "" } }] },
+        },
+      });
+      const replacementSubcomponents = req.body.subcomponents.map((e: Partial<SubjectSubcomponent>) => {
+        return { ...e, componentId: undefined };
+      });
+      const resultdata = await primsa.subjectComponent.update({
+        where: { id: targetComponent.id },
+        data: {
           subcomponents: {
-            updateMany: replacementSubcomponents,
+            createMany: {
+              data: replacementSubcomponents,
+            },
           },
         },
         include: { subcomponents: true },
       });
+
       if (resultdata) return res.status(200).json(resultdata);
       return res.status(500);
-    } else {
-      return res.status(200).json(initupd);
     }
+    return res.status(200).send({});
   }
 });
