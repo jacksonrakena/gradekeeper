70118b5c27dafe4c150263a088977cf0e74a57ff
Jackson Rakena <jackson@rakena.co.nz>
2022-06-21 18:46:43 +1200

    Add database maintenance SQL queries

---
+++ database_maintenance/delete_dangling_relations.sql
@@ -0,0 +1,17 @@
+
+/**
+  This script deletes 'dangling' entities in the database.
+  Theoretically, there should never be any dangling entities,
+  because Prisma is configured to cascade-delete when deleting.
+  But sometimes a bug in a developer environment can fuck everything up,
+  so this script will delete all entities where the parent entity ID column
+  does not have a valid row in the parent table.
+ */
+
+delete from Subject where Subject.id in
+                          (select * from (select Subject.id from Subject
+                              LEFT JOIN StudyBlock
+                                  on StudyBlock.id = Subject.studyBlockId
+                                             WHERE StudyBlock.id IS NULL)temp_table);
+delete from SubjectComponent where SubjectComponent.id in (select * from (select SubjectComponent.id from SubjectComponent LEFT JOIN Subject on Subject.id = SubjectComponent.subjectId WHERE Subject.id IS NULL)temp_table);
+delete from SubjectSubcomponent where SubjectSubcomponent.id in (select * from (select SubjectSubcomponent.id from SubjectSubcomponent LEFT JOIN SubjectComponent on SubjectComponent.id = SubjectSubcomponent.componentId WHERE SubjectComponent.id IS NULL) as SSSCi);
