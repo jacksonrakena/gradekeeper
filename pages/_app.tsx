6d496828c7cec8a79d7ece18e409c3eb5e663c98
Jackson Rakena <jackson@rakena.co.nz>
2022-06-21 12:24:09 +1200

    Fix a strange bug where the mobile viewport scale would be less than 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { SlideFade } from "@chakra-ui/react";
 import { Prisma } from "@prisma/client";
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 import { useEffect, useState } from "react";
 import { Chakra } from "../Chakra";
 import { _undefined } from "../lib/logic";
@@ -20,64 +21,69 @@ function MyApp({ Component, pageProps: { session, ...pageProps }, router }: AppP
       .catch(() => {});
   }, []);
   return (
-    <SessionProvider session={session}>
-      <Chakra cookies={pageProps.cookies}>
-        <UserContext.Provider
-          value={{
-            user: user,
-            setUser: setUser,
-            deleteCourse: (courseId) => {
-              setUser({
-                ...user,
-                gradeMap: user?.gradeMap ?? {},
-                studyBlocks:
-                  user?.studyBlocks.map((sb) => {
-                    if (sb.subjects.filter((d) => d.id === courseId).length > 0) {
-                      return {
-                        ...sb,
-                        subjects: sb.subjects.filter((subj) => subj.id !== courseId),
-                      };
-                    }
-                    return sb;
-                  }) ?? [],
-              });
-            },
-            redownload: async () => {
-              const d = await fetch("/api/user");
-              const e = await d.json();
-              setUser(e);
-            },
-            updateCourse: (courseId, replacementCourse) => {
-              setUser({
-                ...user,
-                gradeMap: user?.gradeMap ?? {},
-                studyBlocks:
-                  user?.studyBlocks.map((sb) => {
-                    if (sb.id === replacementCourse.studyBlockId) {
-                      if (sb.subjects.filter((aa) => aa.id === courseId).length === 0) {
-                        sb.subjects.push(replacementCourse);
-                        return sb;
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <SessionProvider session={session}>
+        <Chakra cookies={pageProps.cookies}>
+          <UserContext.Provider
+            value={{
+              user: user,
+              setUser: setUser,
+              deleteCourse: (courseId) => {
+                setUser({
+                  ...user,
+                  gradeMap: user?.gradeMap ?? {},
+                  studyBlocks:
+                    user?.studyBlocks.map((sb) => {
+                      if (sb.subjects.filter((d) => d.id === courseId).length > 0) {
+                        return {
+                          ...sb,
+                          subjects: sb.subjects.filter((subj) => subj.id !== courseId),
+                        };
                       }
-                      return {
-                        ...sb,
-                        subjects: sb.subjects.map((subj) => {
-                          if (subj.id === replacementCourse.id) return replacementCourse;
-                          return subj;
-                        }),
-                      };
-                    }
-                    return sb;
-                  }) ?? [],
-              });
-            },
-          }}
-        >
-          <SlideFade key={router.route} in={true}>
-            <Component {...pageProps} />
-          </SlideFade>
-        </UserContext.Provider>
-      </Chakra>
-    </SessionProvider>
+                      return sb;
+                    }) ?? [],
+                });
+              },
+              redownload: async () => {
+                const d = await fetch("/api/user");
+                const e = await d.json();
+                setUser(e);
+              },
+              updateCourse: (courseId, replacementCourse) => {
+                setUser({
+                  ...user,
+                  gradeMap: user?.gradeMap ?? {},
+                  studyBlocks:
+                    user?.studyBlocks.map((sb) => {
+                      if (sb.id === replacementCourse.studyBlockId) {
+                        if (sb.subjects.filter((aa) => aa.id === courseId).length === 0) {
+                          sb.subjects.push(replacementCourse);
+                          return sb;
+                        }
+                        return {
+                          ...sb,
+                          subjects: sb.subjects.map((subj) => {
+                            if (subj.id === replacementCourse.id) return replacementCourse;
+                            return subj;
+                          }),
+                        };
+                      }
+                      return sb;
+                    }) ?? [],
+                });
+              },
+            }}
+          >
+            <SlideFade key={router.route} in={true}>
+              <Component {...pageProps} />
+            </SlideFade>
+          </UserContext.Provider>
+        </Chakra>
+      </SessionProvider>
+    </>
   );
 }
 
