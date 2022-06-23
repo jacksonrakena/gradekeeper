a291fdbd90d0fe4c4908e14dc98007a7260284fe
Jackson Rakena <jackson@rakena.co.nz>
2022-06-23 16:58:44 +1200

    I really hate Sentry

--- next.config.js
+++ next.config.js
@@ -24,7 +24,6 @@ const sentryWebpackPluginOptions = {
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
 
-  configFile: "./configs/",
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
