9448f6816c2f915932e88a33b9179d32b1e4e845
Jackson Rakena <jackson@rakena.co.nz>
2022-06-30 12:15:08 +1200

    Add indicator on home screen when a course is finished

--- components/app/main/CoursePill.tsx
+++ components/app/main/CoursePill.tsx
@@ -1,10 +1,12 @@
-import { Tag, Text, useColorModeValue } from "@chakra-ui/react";
+import { Icon, Tag, Text, useColorModeValue } from "@chakra-ui/react";
 import { FullSubject } from "../../../lib/fullEntities";
-import { calculateProjectedCourseGrade } from "../../../lib/logic";
+import { calculateIsCourseCompleted, calculateProjectedCourseGrade } from "../../../lib/logic";
 import themeConstants from "../../../lib/theme/themeConstants";
+import { BsCheckCircleFill } from "react-icons/bs";
 
 const CoursePill = (props: { subject: FullSubject; gradeMap: any; onClick: () => any }) => {
   const grade = calculateProjectedCourseGrade(props.subject, props.gradeMap);
+  const completed = calculateIsCourseCompleted(props.subject);
   const subject = props.subject;
   return (
     <div
@@ -25,26 +27,42 @@ const CoursePill = (props: { subject: FullSubject; gradeMap: any; onClick: () =>
         </span>
         <span style={{ fontWeight: "bold" }}>{subject.longName} </span>
       </Text>
-      <div className="flex">
-        <div style={{ textAlign: "center" }} className="flex flex-col">
-          {grade.numerical === 0 ? (
-            <Tag px={3} my={3} colorScheme={"teal"}>
-              No data
-            </Tag>
-          ) : (
-            <>
-              <span style={{ fontWeight: "bold" }}>{grade.letter}</span>
-              <span>{(grade.numerical * 100).toFixed(2)}%</span>
-            </>
-          )}
-        </div>
+      {!completed ? (
+        <>
+          <div className="flex">
+            <div style={{ textAlign: "center" }} className=" flex flex-col">
+              {grade.numerical === 0 ? (
+                <Tag px={3} my={3} colorScheme={"teal"}>
+                  No data
+                </Tag>
+              ) : (
+                <>
+                  <span style={{ fontWeight: "bold" }}>{grade.letter}</span>
+                  <span>{(grade.numerical * 100).toFixed(2)}%</span>
+                </>
+              )}
+            </div>
 
-        <div className="py-3 px-4 flex grow">
-          <div style={{ backgroundColor: "#D9D9D9" }} className="rounded flex grow">
-            <div style={{ backgroundColor: subject.color, width: "" + grade.numerical * 100 + "%" }} className="rounded"></div>
+            <div className="py-3 px-4 flex grow">
+              <div style={{ backgroundColor: "#D9D9D9" }} className="rounded flex grow">
+                <div style={{ backgroundColor: subject.color, width: "" + grade.numerical * 100 + "%" }} className="rounded"></div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <>
+          <div className="flex items-center justify-center">
+            <div className="flex items-center">
+              <Icon color={subject.color} as={BsCheckCircleFill} w={4} h={4} mr={2} />
+              <Text style={{ fontWeight: "bold" }} mr={2}>
+                {grade.letter}
+              </Text>
+              <Text>{(grade.numerical * 100).toFixed(2)}%</Text>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
