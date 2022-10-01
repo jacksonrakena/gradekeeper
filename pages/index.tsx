c60d2fb164d5af49533e32724f06523cdfb938a4
jacksonrakena <jackson@rakena.co.nz>
2022-10-01 13:49:12 +1300

    Apply invalidators to new state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
-import { Prisma } from "@prisma/client";
-import { NextPage } from "next";
+import { PrismaClient } from "@prisma/client";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { unstable_getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useSetRecoilState } from "recoil";
 import Dashboard from "../components/app/dashboard/Dashboard";
 import MarketingHome from "../components/marketing/MarketingHome";
-import { GetUserResponse } from "../state/course";
-import { getUserQuery } from "./api/users/me";
+import { UserState } from "../state/course";
+import { nextAuthOptions } from "./api/auth/[...nextauth]";
+import { getUserInfo } from "./api/users/me";
 
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await unstable_getServerSession(context.req, context.res, nextAuthOptions);
 
-const Home: NextPage = () => {
+  return {
+    props: {
+      initialUser: !session || !session.user?.email ? null : await getUserInfo(new PrismaClient(), session.user.email),
+    },
+  };
+}
+
+const Home = (serverProps: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const data = useSession();
+  const setInitialUserState = useSetRecoilState(UserState);
+  useEffect(() => {
+    if (serverProps.initialUser) {
+      setInitialUserState(serverProps.initialUser);
+    }
+  }, []);
   return (
     <div>
       {(!data || data.status === "unauthenticated") && <MarketingHome />}
