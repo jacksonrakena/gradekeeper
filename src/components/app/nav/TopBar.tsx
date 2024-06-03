f83878e13377a993c35f37e154cf2fbe4f92b5d9
jacksonrakena <jackson@rakena.co.nz>
2024-06-03 20:36:56 +1200

    Topbar cleanup

--- src/components/app/nav/TopBar.tsx
+++ src/components/app/nav/TopBar.tsx
@@ -1,35 +1,15 @@
-import { Button, SkeletonText } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { ReactElement } from "react";
-import { useLogin, useSession } from "../../../lib/state/auth";
+import { useSession } from "../../../lib/state/auth";
 import AccountButton from "./AccountButton";
 
 export const TopBar = (props: { leftWidget?: ReactElement }) => {
-  const login = useLogin();
   const session = useSession();
 
   return (
-    <div>
-      <div className="w-full p-2 flex flex-row">
-        <div className="grow">{props.leftWidget}</div>
-        <div>
-          <SkeletonText isLoaded={!!session}>
-            {session ? (
-              <AccountButton />
-            ) : (
-              <>
-                <Button
-                  colorScheme={"brand"}
-                  onClick={() => {
-                    login();
-                  }}
-                >
-                  Sign in
-                </Button>
-              </>
-            )}
-          </SkeletonText>
-        </div>
-      </div>
-    </div>
+    <Flex w={"full"} p={2}>
+      <Box flexGrow={1}>{props.leftWidget}</Box>
+      <Box>{session && <AccountButton />}</Box>
+    </Flex>
   );
 };
