fc81124394c102969c40459e3dcf30d03b244b74
Jackson <jackson@rakena.co.nz>
2024-06-05 19:08:08 +1200

    Update block-templates.tsx

--- src/lib/data/block-templates.tsx
+++ src/lib/data/block-templates.tsx
@@ -7,12 +7,12 @@ export const templates: { [key: string]: { name: string; startDate: string; endD
     },
     {
       name: "Trimester 2, 2024",
-      startDate: "2023-07-08",
-      endDate: "2023-11-09",
+      startDate: "2024-07-08",
+      endDate: "2024-11-09",
     },
     {
       name: "Trimester 3, 2023",
-      startDate: "2023-11-11",
+      startDate: "2024-11-11",
       endDate: "2025-02-16",
     },
   ],
