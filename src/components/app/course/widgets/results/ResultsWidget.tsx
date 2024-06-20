b09cd7d5903efbe028dc51f63c9a3a9b463d96b6
jacksonrakena <jackson@rakena.co.nz>
2024-06-20 17:37:51 +1200

    oops

--- src/components/app/course/widgets/results/ResultsWidget.tsx
+++ src/components/app/course/widgets/results/ResultsWidget.tsx
@@ -28,11 +28,12 @@ export const ResultsWidget = (props: { course: ProcessedCourse }) => {
     if (!over) return;
     if (active.id !== over.id) {
       (async () => {
-        const resultant = arrayMove(
-          comp,
-          props.course.components.findIndex((e) => e.id === active.id),
-          props.course.components.findIndex((e) => e.id === over.id)
-        );
+        const first = props.course.components.filter((e) => e.id === active.id)[0];
+        const second = props.course.components.filter((e) => e.id === over.id)[0];
+
+        const fi = comp.indexOf(first);
+        const si = comp.indexOf(second);
+        const resultant = arrayMove(comp, fi, si);
         setComp(resultant);
         const reset = () => {
           setComp(props.course.components.toSorted((a, b) => (a.sequenceNumber ?? 0) - (b.sequenceNumber ?? 0)));
