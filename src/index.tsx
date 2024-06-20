8c81c3cde2fcb0f4367dc1e6e50e4b2d4a6da1b1
jacksonrakena <jackson@rakena.co.nz>
2024-06-20 14:33:23 +1200

    Enable strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { Provider } from "jotai";
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../styles/globals.css";
 import { AppRoot } from "./app";
@@ -7,7 +7,9 @@ import { AppRoot } from "./app";
 console.log("Gradekeeper Javascript client (c) Jackson Rakena, initialising", new Date());
 
 createRoot(document.getElementById("root")!).render(
-  <Provider>
-    <AppRoot />
-  </Provider>
+  <StrictMode>
+    <Provider>
+      <AppRoot />
+    </Provider>
+  </StrictMode>
 );
