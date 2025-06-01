687a637b68369a69f5581200f7347689a56fb840
Jackson <jackson@rakena.co.nz>
2025-06-01 20:38:36 +1000

    Update Footer.tsx

--- src/components/app/Footer.tsx
+++ src/components/app/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <Box color={"GrayText"} fontSize={"sm"}>
       &copy; Jackson Rakena <br />
-      <Link href="https://redirects.jacksonrakena.com/discord">Join our Discord for support</Link> &bull;{" "}
+      <Link href="https://discord.gg/jpgrtS6UsV">Join our Discord for support</Link> &bull;{" "}
       <Link href="/legal/privacy">Privacy</Link> &bull; <Link href="/legal/donate">Donate</Link>
     </Box>
   );
